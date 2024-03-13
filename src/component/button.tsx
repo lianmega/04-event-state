7129da33595c67ba775bbdef40dbf97458b1edb7
Lian2041720257 <2041720257@student.polinema.ac.id>
2024-03-13 13:26:06 +0700

    Praktikum 1

---
+++ src/component/button.tsx
@@ -0,0 +1,7 @@
+export default function Tombol_1(){
+    return(
+        <button className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded">
+            Ini tombol
+        </button>
+    );
+}
